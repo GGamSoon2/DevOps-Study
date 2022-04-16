6b1ddb4331959fdb9399f18a2ba5b9ddd3dcaa1e
GGamSoon2 <masters4@naver.com>
2022-04-16 16:48:15 +0000

    ec2 create

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
   }
 
   cloud {
-    organization = "ggamsoon2"
+    organization = "<ggamsoon2>"
 
     workspaces {
       name = "tf-aws-ec2-git-action"
@@ -18,34 +18,34 @@ terraform {
 }
 
 provider "aws" {
-  region     = "ap-northeast-2"
+  region = "ap-northeast-2"
 }
 
 resource "aws_instance" "terrafrom-ec2" {
-  ami           = "ami-0ed11f3863410c386"
-  instance_type = "t2.micro"
-  subnet_id = "subnet-0999184b37219db7d"
+  ami                         = "ami-0ed11f3863410c386"
+  instance_type               = "t2.micro"
+  subnet_id                   = "subnet-0999184b37219db7d"
   associate_public_ip_address = "true"
-  security_groups = ["sg-078cf2eafc71b9688"]
+  security_groups             = ["sg-078cf2eafc71b9688"]
 }
 
 resource "aws_security_group" "default_in_allow_ssh" {
   name        = "allow_ssh"
   description = "Allow ssh inbound traffic"
-  vpc_id      = "vpc-07dea6e36a1320411" 
+  vpc_id      = "vpc-07dea6e36a1320411"
 
   ingress {
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
