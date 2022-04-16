20650651eaff6fdf5128fc409583af871858c006
GGamSoon2 <masters4@naver.com>
2022-04-16 17:02:08 +0000

    terraform ec2 auto create

--- main.tf
+++ main.tf
@@ -17,18 +17,6 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = "ap-northeast-2"
-}
-
-resource "aws_instance" "terrafrom-ec2" {
-  ami                         = "ami-0ed11f3863410c386"
-  instance_type               = "t2.micro"
-  subnet_id                   = "subnet-0999184b37219db7d"
-  associate_public_ip_address = "true"
-  security_groups             = ["sg-078cf2eafc71b9688"]
-}
-
 resource "aws_security_group" "default_in_allow_ssh" {
   name        = "allow_ssh"
   description = "Allow ssh inbound traffic"
@@ -51,4 +39,16 @@ resource "aws_security_group" "default_in_allow_ssh" {
   tags = {
     Name = "allow"
   }
+}
+
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+resource "aws_instance" "terrafrom-ec2" {
+  ami                         = "ami-0ed11f3863410c386"
+  instance_type               = "t2.micro"
+  subnet_id                   = "subnet-0999184b37219db7d"
+  associate_public_ip_address = "true"
+  security_groups             = ["sg-078cf2eafc71b9688"]
 }
